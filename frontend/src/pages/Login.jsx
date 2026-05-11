88f080ee594ef44a93bed18b4df9dd0b309eb855
Soham-Lodh <sohamlodh06@gmail.com>
2026-05-11 18:05:05 +0530

    updates

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -646,10 +646,6 @@ const Login = () => {
 
             <div
               className="switch-text"
-              data-aos="fade-up"
-              data-aos-duration="500"
-              data-aos-delay="520"
-              data-aos-once="true"
             >
               {state === "Sign Up" ? (
                 <p>
